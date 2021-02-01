92a9bd99f725e1c58f16121b4b174edc74d23d9c
Aibhstin <aibhsec@protonmail.com>
2021-02-01 13:27:00 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ fn main() {
     loop {
         let mut c = [0];
 
-        while let Ok(_) = stdin().read(&mut c) {;}
+        while let Ok(_) = stdin().read(&mut c) {
+            if c == ['q' as u8] {
+                exit(1);
+            }
+        }
     }
 
-    exit(1);
+    exit(0);
 }
